abb84a5f97915d833254401e6dabd2a11934b594
ety001 <ety001@domyself.me>
2013-09-04 17:14:48 +0800

    添加视频功能

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,5 @@
 <?php
 //去除编辑器中输入的特殊标记
 function strip_illegal_tags($t){
-    return strip_tags($t,'<p><a><div><ul><li><ol><img><span><h1><h2><h3><h4><h5><h6>');
+    return strip_tags($t,'<p><a><div><ul><li><ol><img><span><h1><h2><h3><h4><h5><h6><embed>');
 }
