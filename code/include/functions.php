8b2f3b1ae0f8bf75dfc1c00c234a1408001b5786
ety001 <ety001@domyself.me>
2013-09-10 10:41:24 +0800

    增加设备识别函数

--- code/include/functions.php
+++ code/include/functions.php
@@ -45,4 +45,15 @@ function time_ago($time) {
         $retrun = $time_deff.'秒前'; 
     } 
     return $retrun; 
-} 
+}
+//获取设备类型
+function get_device_type(){
+    $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+    $pattern = '/ipad|iphone|android/';
+    preg_match($pattern, $agent, $matches);
+    if($matches[0]){
+        return false;
+    } else {
+        return true;
+    }
+}
