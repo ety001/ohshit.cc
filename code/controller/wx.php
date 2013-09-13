4bf271ee2df66ee63d61bbb3757eca10ca19ca75
ety001 <ety001@domyself.me>
2013-09-13 13:30:53 +0800

    来自微信的字样换成微信的logo，增加发图片的功能[未测试]

--- code/controller/wx.php
+++ code/controller/wx.php
@@ -10,7 +10,7 @@ class wx extends spController
                     $this->textType($msg,$wx);
                     break;
                 case 'image':
-                    # code...
+                    $this->imageType($msg,$wx);
                     break;
                 case 'location':
                     # code...
@@ -24,17 +24,34 @@ class wx extends spController
 	}
 
     private function textType($msg,$wx){
+        $memInfo = spAccess('r',$msg['FromUserName']);
+        $mem = '';
+        if($memInfo){
+            spAccess('c', $msg['FromUserName']);
+            switch ($memInfo['MsgType']) {
+                case 'image':
+                    $mem = '<p><img src="'.$memInfo['PicUrl'].'" class="img-polaroid"></p>';
+                    break;
+                case 'location':
+                    $mem = '<p></p>';
+                    break;
+                
+                default:
+                    $mem = '';
+                    break;
+            }
+        }
         $wxDesp = '<p>这是一条来自微信端的消息，如果你也想发送，请微信搜索添加 dmrobot ，或者扫描用微信下面的二维码</p><p><img src="/public/img/wx.jpg" class="img-polaroid"></p>';
         $now = time();
         $length = strlen($msg['Content']);
         if($length < 100){
             $title = $msg['Content'];
-            $content = $wxDesp;
+            $content = $mem.$wxDesp;
         } else {
             $title = cut_str($msg['Content'], 90, 0); ;
-            $content = $msg['Content'].'<hr>'.$wxDesp;
+            $content = $mem.$msg['Content'].'<hr>'.$wxDesp;
         }
-        $title = '[来自微信]'.$title;
+        $title = '<img src="/public/img/wx-logo.png" class="px28 r_margin">'.$title;
         $info = array(
             'uid'=>0,
             'title'=>strip_illegal_tags($title),
@@ -49,5 +66,10 @@ class wx extends spController
         } else {
             echo $wx->replyText('发布失败');
         }
+    }
+
+    private function imageType($msg,$wx){
+        spAccess('w' , $msg['FromUserName'], $msg, 3600);
+        $wx->replyText('请在1小时内输入你要给图片的备注文字信息');
     }
 }
